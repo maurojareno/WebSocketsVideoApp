31212115fe1340b617b7a9e433c92b6c98863c42
maurojareno <maurojareno@gmail.com>
2020-11-03 01:34:47 -0300

    fix deploy: imports > require

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import { VideoServer } from './src/videoServer';
-import { Routes } from './src/routes';
+// import { VideoServer } from './src/videoServer';
+// import { Routes } from './src/routes';
+var vs = require('./src/videoServer');
+var routes = require('./src/routes');
 
-let app = new VideoServer().getApp();
-const route = new Routes(app);
+let app = new vs.VideoServer().getApp();
+const route = new routes.Routes(app);
 route.home();
 
-export { app };
